989c3b7b8e50bb98fef50d4ab75afcbca41662cd
Joan Cuch <stauas@gmail.com>
2018-05-12 21:52:19 +0200

    Fixed Comments database

---
+++ Not Only Games Project/Assets/Scripts/CommentsManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CommentsManager : MonoBehaviour {
+
+    public ReadComments i_readComments;
+
+    List<string> comments;
+
+	// Use this for initialization
+	void Start () {
+        i_readComments.ReadString();
+        comments = i_readComments.GetComments('6');    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Not Only Games Project/Assets/Scripts/ReadComments.cs
+++ Not Only Games Project/Assets/Scripts/ReadComments.cs
@@ -36,9 +36,10 @@ public class ReadComments : MonoBehaviour {
 
         for (int idx = 0; idx < l_CommentsString.Length; idx++)
         {
-            if (l_CommentsString[idx] == '#' && idx != l_CommentsString.Length-1) vAux_substringCount++;
+            
+                if (l_CommentsString[idx] == '#' && idx < l_CommentsString.Length-1) vAux_substringCount++;
+            
         }
-
         //Instantiate a string[] of the define number of strings
         m_commentsArray = new string[vAux_substringCount];
         vAux_substringCount = 0;
@@ -63,14 +64,11 @@ public class ReadComments : MonoBehaviour {
 
         for (int idx = 0; idx < m_commentsArray.Length; idx++)
         {
-            
-
             if (m_commentsArray[idx][0] == sceneChar)
             {
                 string newComment = m_commentsArray[idx].Substring(1, m_commentsArray[idx].Length - 2);
                 l_commentsList.Add(newComment);
-            }
-                
+            }            
         }
         return l_commentsList;
     }
