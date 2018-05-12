80540535adecc4f2fe47e291df58c4d5615a67c3
Alaindelon98 <alanelya@gmail.com>
2018-05-12 20:55:25 +0200

    Script likes vacio, y guia de numero s en la carpeta de git

---
+++ Not Only Games Project/Assets/Scripts/LikesFollowers.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LikesFollowers : MonoBehaviour {
+
+    public int[] likesArray, followersArray;
+
+    [SerializeField]
+    Text likesText, followersText;
+	void Start () {
+		
+	}
+
+    void Update () {
+		
+	}
+}
