b8b039f29efe2e84492f410ca3fe63a53c47ef0a
Ricard <r.cabezas.diez@gmail.com>
2018-05-12 10:48:12 +0200

    Screenshot

---
+++ Not Only Games Project/Assets/movement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour {
+
+    float speed = 2f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 _newPosition = transform.position;
+        _newPosition.x += Mathf.Sin(Time.time) * Time.deltaTime * speed;
+        transform.position = _newPosition;
+
+    }
+}
